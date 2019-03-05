540345a44709cdc45e23033f252974c3ebf74cf6
VN\ducdoan <lovecherub9@gmail.com>
2019-03-05 14:46:09 +0700

    Update package name

--- src/android/GoToSettingsPlugin.java
+++ src/android/GoToSettingsPlugin.java
@@ -1,4 +1,4 @@
-package cordova-plugin-go-to-settings;
+package cordova-plugin-go-to-settings.GoToSettingsPlugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;

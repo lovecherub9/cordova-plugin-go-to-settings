25fdd119748297a279f637ffbb3df9cf08be5f99
Duc <lovecherub9@gmail.com>
2019-03-05 13:52:02 +0700

    Init

---
+++ src/android/GoToSettingsPlugin.java
@@ -0,0 +1,23 @@
+package cordova-plugin-go-to-settings;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class GoToSettingsPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS));
+            return true;
+        }
+        return false;
+    }
+}
